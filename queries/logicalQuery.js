6473e7741fec681bbcdfd6602a85888859123ddd
asifalam515 <asibulalam515@gmail.com>
2025-01-03 15:21:40 +0600

    or logic added

--- queries/logicalQuery.js
+++ queries/logicalQuery.js
@@ -3,3 +3,12 @@ db.test.find({$and: [
     {age:{$ne: 15}},
     {age: {$lte: 30 }},
     ]})
+
+
+// another example using logic or
+db.test.find({
+    $or: [
+        {interests:"Reading"},
+        {interests:"Travelling"}
+        ]
+ },{age:1,name:1,interests:1}).sort({ sortField:1 })
