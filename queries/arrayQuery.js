bb48c037835bfad3ac100f91b62205be5f168573
asifalam515 <asibulalam515@gmail.com>
2025-01-03 20:48:42 +0600

    array query added

---
+++ queries/arrayQuery.js
@@ -0,0 +1,21 @@
+Array Query Operator::
+{ <field>: { $all: [ <value1> , <value2> ... ] } }
+db.test.find({interests:{$all:["Travelling","Reading"]}})
+
+#another example
+db.test.find({"skills.name":"JAVA"})
+
+#Elem Match: will give all the matched item(doesn't matter order) 
+{ <field>: { $elemMatch: { <query1>, <query2>, ... } } }
+db.test.find({
+    skills:{$elemMatch: {
+        name:"JAVASCRIPT",
+        level:"Expert",
+        // isLearning:"false"
+    }
+    
+} })
+   .projection({})
+   .sort({$natural:-1})
+   .limit(0)
+
