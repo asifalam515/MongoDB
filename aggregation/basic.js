1c8d1263e4a01cfe5aa992c014b1facf1f564fa5
asifalam515 <asibulalam515@gmail.com>
2025-01-06 08:40:38 +0600

    stage 2 added

--- aggregation/basic.js
+++ aggregation/basic.js
@@ -1,5 +1,7 @@
 db.test.aggregate([
     //stage 1:
-    {$match: {gender:"Male",age:{$lt:30}}}
+    {$match: {gender:"Male",age:{$lt:30}}};
+    // stage2
+    {$project:{name:1,age:1}};
     ])
     
