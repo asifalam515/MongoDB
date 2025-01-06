586b03393dc520f4aa02a66920dd21b0b1f2f5bb
asifalam515 <asibulalam515@gmail.com>
2025-01-06 11:39:53 +0600

    bucketing

--- aggregation/bucket.js
+++ aggregation/bucket.js
@@ -1,15 +1,24 @@
 db.test.aggregate([
-    //stage 1:
+    // Stage 1: Bucketing
     {
         $bucket: {
             groupBy: "$age",
             boundaries: [20, 40, 60, 80],
             default: "80+ age group of people",
             output: {
-                count: { $sum: 1 }
+                count: { $sum: 1 },
+                peopleDoc: { $push: "$$ROOT" }
             }
-
+        }
+    },
+    // Stage 2: Sorting
+    {
+        $sort: { count: -1 }
+    },
+    // Stage 3: Project
+    {
+        $project: {
+            count: 1
         }
     }
-
-])
+]);
