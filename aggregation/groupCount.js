b3a05a6c22c637855cd615b9642a655c00d41221
asifalam515 <asibulalam515@gmail.com>
2025-01-06 10:03:12 +0600

    avarage salary counted

--- aggregation/groupCount.js
+++ aggregation/groupCount.js
@@ -2,3 +2,12 @@ db.test.aggregate([
     //stage 1:
     {$group: { _id: "$gender"}}
     ])
+
+    // avarage of salary:
+    db.test.aggregate([
+    
+        //stage 1:
+        {$group: { _id: null, totalDoc:{$sum:1},avgSalary:{$avg: "$salary"}}
+           
+        }
+        ])
