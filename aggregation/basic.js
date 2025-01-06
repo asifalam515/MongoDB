7162a2bad62e6172eba83fc8c4c60ebd481b47ac
asifalam515 <asibulalam515@gmail.com>
2025-01-06 09:22:07 +0600

    group added

--- aggregation/basic.js
+++ aggregation/basic.js
@@ -1,8 +1,8 @@
 db.test.aggregate([
     //stage 1:
-    {$match: {gender:"Male",age:{$lt:30}}};
+    {$match: {gender:"Male",age:{$lt:30}}},
     // stage2
-    {$project:{name:1,age:1}};
+    {$project:{name:1,age:1}}
     ])
     
     // another query
@@ -10,9 +10,22 @@ db.test.aggregate([
         //stage 1:
         { $match: { gender: "Male", age: { $lt: 30 } } },
         //stage 2:
-        {$addFields: {course:"How to handle Women"}}
+        {$addFields: {course:"How to handle Women"}},
         //stage 3:
         {$project:{course:1}}
         
     ])
+
+    // create another pipline 
+    db.test.aggregate([
+        //stage 1:
+        { $match: { gender: "Male", age: { $lt: 30 } } },
+        //stage 2:
+        {$addFields: {course:"How to handle Women",edTech:"asif's online course"}},
+        // stage 3:
+        {$project: {course:1,edTech:1}},
+        // stage 4:
+        {$out: "course_student"}
+        ])
+    
     
