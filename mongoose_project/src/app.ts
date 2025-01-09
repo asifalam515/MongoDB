b598ff7b30784b26679739e8d99a6c9a21df1a3a
asifalam515 <asibulalam515@gmail.com>
2025-01-09 19:58:37 +0600

    setup done

--- mongoose_project/src/app.ts
+++ mongoose_project/src/app.ts
@@ -1,8 +1,13 @@
-import express, { Application, Request, Response } from 'express';
-const app:Application = express()
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+//parse
+app.use(express.json())
+app.use(cors())
+app.get('/', (req: Request, res: Response) => {
+  var a = 10
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+  res.send(a)
 })
 console.log(process.cwd())
 export default app
--- mongoose_project/src/server.ts
+++ mongoose_project/src/server.ts
@@ -1,18 +1,15 @@
-import app from "./app"
-import config from "./app/config";
-const mongoose = require('mongoose');
-
-
+import app from './app'
+import config from './app/config'
+import mongoose from 'mongoose'
 
 async function main() {
-  await mongoose.connect(config.database_url);
+  try {
+    await mongoose.connect(config.database_url as string)
 
- 
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
-
-
-
-
-app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`)
-  })
